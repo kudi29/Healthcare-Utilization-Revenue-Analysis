95658c227d70badc7165438ac97f41a02c240a70
Wadahfred Kudi <wadahfred29@gmail.com>
2026-03-11 12:21:19 -0400

    Update hospital_analytics_cleaning_queries.sql

--- scripts/hospital_analytics_cleaning_queries.sql
+++ scripts/hospital_analytics_cleaning_queries.sql
@@ -3,8 +3,8 @@ CREATE SCHEMA IF NOT EXISTS analytics;
 -- =============================================
 -- 1. Analytics Patients Dimension Table
 -- =============================================
-
 DROP TABLE IF EXISTS analytics.dim_patients CASCADE;
+
 CREATE TABLE analytics.dim_patients (
     id TEXT,
     birth_date DATE,
@@ -38,44 +38,43 @@ INSERT INTO analytics.dim_patients (
     gender, place_of_birth, address, city, state, county, fips,
     zip_code, latitude, longitude, healthcare_expenses, healthcare_coverage, income
 )
-
 SELECT
-	id,
-	birthdate::DATE,
-	deathdate::DATE,
-	ssn,
-	COALESCE(drivers, 'Unknown') AS drivers,
-	COALESCE(passport, 'Unknown'),
-	REGEXP_REPLACE(first, '[0-9]', '', 'g') AS first_name,
-	REGEXP_REPLACE(middle, '[0-9]', '', 'g') AS middle_name,
-	REGEXP_REPLACE(last, '[0-9]', '', 'g') AS last_name,
-	CASE UPPER(marital)
-		WHEN 'S' THEN 'Single'
-		WHEN 'M' THEN 'Married'
-		WHEN 'D' THEN 'Divorced'
-		WHEN 'W' THEN 'Widowed'
-		ELSE  'Unknown'	
-	END AS marital_status,	
-	race,
-	CASE UPPER(gender)
-		WHEN 'M' THEN 'Male'
-		WHEN 'F' THEN 'Feale'
-		ELSE 'Unknown'
-	END AS gender,	
-	birthplace,
-	address,
-	city,
-	state,
-	county,
-	fips,
-	zip AS zip_code,
-	CAST(lat AS DOUBLE PRECISION) AS latitude,
-	CAST(lon AS DOUBLE PRECISION)longitude,
-	CAST(healthcare_expenses AS NUMERIC(12,2)),
-	CAST(healthcare_coverage AS NUMERIC(12,2)),
-	CAST(income	AS NUMERIC(12,2))
-FROM raw.patients;
-
+    id,
+    birthdate::DATE AS birth_date,
+    deathdate::DATE AS death_date,
+    ssn,
+    COALESCE(drivers, 'Unknown') AS drives,
+    COALESCE(passport, 'Unknown') AS passport,
+    TRIM(REGEXP_REPLACE(first, '[0-9]', '', 'g')) AS first_name,
+    TRIM(REGEXP_REPLACE(middle, '[0-9]', '', 'g')) AS middle_name,
+    TRIM(REGEXP_REPLACE(last, '[0-9]', '', 'g')) AS last_name,
+    CASE UPPER(marital)
+        WHEN 'S' THEN 'Single'
+        WHEN 'M' THEN 'Married'
+        WHEN 'D' THEN 'Divorced'
+        WHEN 'W' THEN 'Widowed'
+        ELSE 'Unknown'
+    END AS marital_status,
+    race,
+    CASE UPPER(gender)
+        WHEN 'M' THEN 'Male'
+        WHEN 'F' THEN 'Female'
+        ELSE 'Unknown'
+    END AS gender,
+    birthplace,
+    address,
+    city,
+    state,
+    county,
+    fips,
+    zip AS zip_code,
+    CAST(lat AS DOUBLE PRECISION) AS latitude,
+    CAST(lon AS DOUBLE PRECISION) AS longitude,
+    CAST(GREATEST(healthcare_expenses,0) AS NUMERIC(12,2)) AS healthcare_expenses,
+    CAST(GREATEST(healthcare_coverage,0) AS NUMERIC(12,2)) AS healthcare_coverage,
+    CAST(GREATEST(income,0) AS NUMERIC(12,2)) AS income
+FROM raw.patients
+WHERE birthdate <= CURRENT_DATE; 
 
 -- =============================================
 -- 2. Analytics Providers Dimension Table
@@ -86,42 +85,40 @@ DROP TABLE IF EXISTS analytics.dim_providers CASCADE;
 CREATE TABLE analytics.dim_providers (
     id TEXT,
     organization TEXT,
-    name TEXT,
+    full_name TEXT,
     gender TEXT,
     speciality TEXT,
     address TEXT,
     city TEXT,
     state TEXT,
-    zip TEXT,
+    zip_code TEXT,
     latitude DOUBLE PRECISION,
     longitude DOUBLE PRECISION,
-    encounters TEXT
+    encounters INT
 );
 
 INSERT INTO analytics.dim_providers (
-    id, organization, name, gender, speciality, address, city, state, zip,
+    id, organization, full_name, gender, speciality, address, city, state, zip_code,
     latitude, longitude, encounters
 )
-
 SELECT
-	id,
-	organization,
-	REGEXP_REPLACE(name, '[0-9]', '', 'g') AS full_name,
-	CASE UPPER(gender)
-		WHEN 'M' THEN 'Male'
-		WHEN 'F' THEN 'Feale'
-		ELSE 'Unknown'
-	END AS gender,
-	INITCAP(speciality) AS speciality,
-	address,
-	city,
-	state,
-	zip AS zip_code,
-	CAST(lat AS DOUBLE PRECISION) AS latitude,
-	CAST(lon AS DOUBLE PRECISION) AS longitude,
-	CAST (encounters AS INT) AS ecounters
+    id,
+    organization,
+    TRIM(REGEXP_REPLACE(name, '[0-9]', '', 'g')) AS full_name,
+    CASE UPPER(gender)
+        WHEN 'M' THEN 'Male'
+        WHEN 'F' THEN 'Female'
+        ELSE 'Unknown'
+    END AS gender,
+    INITCAP(speciality) AS speciality,
+    address,
+    city,
+    state,
+    zip AS zip_code,
+    CAST(lat AS DOUBLE PRECISION) AS latitude,
+    CAST(lon AS DOUBLE PRECISION) AS longitude,
+    CAST(GREATEST(encounters,0) AS INT) AS encounters
 FROM raw.providers;
-
 
 -- =============================================
 -- 3. Analytics Encounters Fact Table
@@ -129,7 +126,7 @@ FROM raw.providers;
 DROP TABLE IF EXISTS analytics.fact_encounters CASCADE;
 
 CREATE TABLE analytics.fact_encounters (
-    id TEXT,
+    id TEXT PRIMARY KEY,
     start_date DATE,
     end_date DATE,
     patient_id TEXT,
@@ -145,23 +142,10 @@ CREATE TABLE analytics.fact_encounters (
     reason_description TEXT
 );
 
-
-
 INSERT INTO analytics.fact_encounters (
-    id,
-    start_date,
-    end_date,
-    patient_id,
-    organization,
-    provider_id,
-    payer,
-    encounter_class,
-    description,
-    base_encounter_cost,
-    total_claim_cost,
-    payer_coverage,
-    reason_code,
-    reason_description
+    id, start_date, end_date, patient_id, organization, provider_id, payer,
+    encounter_class, description, base_encounter_cost, total_claim_cost,
+    payer_coverage, reason_code, reason_description
 )
 SELECT
     id,
@@ -173,29 +157,25 @@ SELECT
     payer,
     encounter_class,
     description,
-    CAST(base_encounter_cost AS NUMERIC(12,2)) AS base_encounter_cost,
-    CAST(total_claim_cost AS NUMERIC(12,2)) AS total_claim_cost,
-    CAST(payer_coverage AS NUMERIC(12,2)) AS payer_coverage,
+    CAST(GREATEST(base_encounter_cost,0) AS NUMERIC(12,2)) AS base_encounter_cost,
+    CAST(GREATEST(total_claim_cost,0) AS NUMERIC(12,2)) AS total_claim_cost,
+    CAST(GREATEST(payer_coverage,0) AS NUMERIC(12,2)) AS payer_coverage,
     CAST(reason_code AS BIGINT) AS reason_code,
     COALESCE(reason_description, 'Unknown') AS reason_description
 FROM raw.encounters e
 WHERE e.patient IN (SELECT id FROM analytics.dim_patients)
-AND e.provider IN (SELECT id FROM analytics.dim_providers);
-
+  AND e.provider IN (SELECT id FROM analytics.dim_providers);
 
 -- =============================================
 -- 4. Analytics Conditions Fact Table
 -- =============================================
 DROP TABLE IF EXISTS analytics.fact_conditions CASCADE;
+
 CREATE TABLE analytics.fact_conditions AS
 SELECT DISTINCT c.*
 FROM raw.conditions c
 WHERE c.patient_id IN (SELECT id FROM analytics.dim_patients)
-AND c.encounter_id IN (SELECT id FROM analytics.fact_encounters);
-
-ALTER TABLE analytics.fact_conditions
-ADD CONSTRAINT pk_fact_conditions PRIMARY KEY (patient_id, encounter_id, start_date, code);
-
+	AND c.encounter_id IN (SELECT id FROM analytics.fact_encounters);
 
 -- =============================================
 -- 5. Analytics Claim Transactions Fact Table
@@ -211,7 +191,6 @@ SELECT
     patient_id,
     INITCAP(type) AS type,
     COALESCE(amount::NUMERIC(12,2), 0) AS amount,
-
     CASE
         WHEN method = 'CHECK' THEN 'Check'
         WHEN method = 'ECHECK' THEN 'E-Check'
@@ -221,42 +200,32 @@ SELECT
         WHEN method IS NULL THEN 'Unknown'
         ELSE 'Other'
     END AS method,
-
     from_date::DATE AS from_date,
     to_date::DATE AS to_date,
     place_of_service,
     procedure_code::BIGINT AS procedure_code,
-
     COALESCE(modifier_1, 'Unknown') AS modifier_1,
     COALESCE(modifier_2, 'Unknown') AS modifier_2,
-
     COALESCE(diagnosis_ref_1::SMALLINT, 0) AS diagnosis_ref_1,
     COALESCE(diagnosis_ref_2::SMALLINT, 0) AS diagnosis_ref_2,
     COALESCE(diagnosis_ref_3::SMALLINT, 0) AS diagnosis_ref_3,
     COALESCE(diagnosis_ref_4::SMALLINT, 0) AS diagnosis_ref_4,
-
     units::SMALLINT AS units,
     department_id::INT AS department_id,
     notes,
-
     unit_amount::NUMERIC(12,2) AS unit_amount,
-
     COALESCE(transfer_out_id::INT, 0) AS transfer_out_id,
     COALESCE(transfers::NUMERIC(12,2), 0) AS transfers,
-
     payments::NUMERIC(12,2) AS payments,
     adjustments::NUMERIC(12,2) AS adjustments,
     outstanding::NUMERIC(12,2) AS outstanding,
-
     appointment_id,
     line_note,
     patient_insurance_id,
     fee_schedule_id,
     provider_id,
     supervising_provider_id
-
 FROM raw.claim_transactions;
-
 
 -- =============================================
 -- 6. Analytics Fact_Claim Table
@@ -264,72 +233,7 @@ FROM raw.claim_transactions;
 
 DROP TABLE IF EXISTS analytics.fact_claims CASCADE;
 
-CREATE TABLE analytics.fact_claims (
-    id TEXT,
-    patient_id TEXT,
-    provider_id TEXT,
-    primary_patient_insurance_id TEXT,
-    secondary_patient_insurance_id TEXT,
-    department_id TEXT,
-    patient_department_id TEXT,
-    diagnosis_1 TEXT,
-    diagnosis_2 TEXT,
-    diagnosis_3 TEXT,
-    diagnosis_4 TEXT,
-    diagnosis_5 TEXT,
-    diagnosis_6 TEXT,
-    diagnosis_7 TEXT,
-    diagnosis_8 TEXT,
-    referring_provider_id TEXT,
-    appointment_id TEXT,
-    current_illness_date DATE,
-    service_date DATE,
-    supervising_provider_id TEXT,
-    status_1 TEXT,
-    status_2 TEXT,
-    status_p TEXT,
-    outstanding_1 NUMERIC(12,2),
-    outstanding_2 NUMERIC(12,2),
-    outstanding_p NUMERIC(12,2),
-    last_billed_date_1 DATE,
-    last_billed_date_2 DATE,
-    last_billed_date_p DATE,
-    healthcare_claim_type_id_1 SMALLINT
-);
-
-
-INSERT INTO analytics.fact_claims (
-    id,
-    patient_id,
-    provider_id,
-    primary_patient_insurance_id,
-    secondary_patient_insurance_id,
-    department_id,
-    patient_department_id,
-    diagnosis_1,
-    diagnosis_2,
-    diagnosis_3,
-    diagnosis_4,
-    diagnosis_5,
-    diagnosis_6,
-    diagnosis_7,
-    diagnosis_8,
-    referring_provider_id,
-    appointment_id,
-    current_illness_date,
-    service_date,
-    supervising_provider_id,
-    status_1,
-    status_2,
-    status_p,
-    outstanding_1,
-    outstanding_2,
-    outstanding_p,
-    last_billed_date_1,
-    last_billed_date_2,
-    last_billed_date_p,
-    healthcare_claim_type_id_1
-)
+CREATE TABLE analytics.fact_claims AS
 SELECT
     id,
     patient_id,
@@ -363,8 +267,7 @@ SELECT
     healthcare_claim_type_id_1::SMALLINT
 FROM raw.claims c
 WHERE c.patient_id IN (SELECT id FROM analytics.dim_patients)
-AND c.provider_id IN (SELECT id FROM analytics.dim_providers);
-
+  AND c.provider_id IN (SELECT id FROM analytics.dim_providers);
 -- =============================================
 -- 7. Add Foreign Key Constraints
 -- =============================================
@@ -407,6 +310,9 @@ CREATE INDEX idx_fact_claims_patient ON analytics.fact_claims(patient_id);
 CREATE INDEX idx_fact_claims_provider ON analytics.fact_claims(provider_id);
 
 
+-- =============================================
+-- 7. Add Primary Key Constraints
+-- =============================================
 
 ALTER TABLE analytics.dim_patients
 ADD CONSTRAINT pk_dim_patients PRIMARY KEY (id);
@@ -414,8 +320,8 @@ ADD CONSTRAINT pk_dim_patients PRIMARY KEY (id);
 
 ALTER TABLE analytics.dim_patients
 ALTER COLUMN id SET NOT NULL,
-ALTER COLUMN first SET NOT NULL,
-ALTER COLUMN last SET NOT NULL;
+ALTER COLUMN first_name SET NOT NULL,
+ALTER COLUMN last_name SET NOT NULL;
 
 
 ALTER TABLE analytics.dim_providers
@@ -423,7 +329,7 @@ ADD CONSTRAINT pk_dim_providers PRIMARY KEY (id);
 
 ALTER TABLE analytics.dim_providers
 ALTER COLUMN id SET NOT NULL,
-ALTER COLUMN name SET NOT NULL;
+ALTER COLUMN full_name SET NOT NULL;
 
 
 ALTER TABLE analytics.fact_encounters
@@ -453,6 +359,7 @@ ALTER TABLE analytics.fact_claims
 ALTER COLUMN id SET NOT NULL,
 ALTER COLUMN patient_id SET NOT NULL,
 ALTER COLUMN provider_id SET NOT NULL;
+
 
 
 
