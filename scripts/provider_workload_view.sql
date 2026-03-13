bcced9deed2bcd09e7e394533a629a5a8996b632
Wadahfred Kudi <wadahfred29@gmail.com>
2026-03-13 09:54:56 +0800

    Update provider workload view to use full_name

--- scripts/provider_workload_view.sql
+++ scripts/provider_workload_view.sql
@@ -7,7 +7,7 @@ Tracks provider workloads by showing how many patients each person is seeing at 
 CREATE VIEW vw_provider_visits AS
 SELECT
     providers.id AS provider_id,
-    INITCAP(providers.name) AS provider_name,
+    INITCAP(providers.full_name) AS provider_name,
     INITCAP(providers.city) AS facility_location,
     COUNT(encounters.id) AS total_patient_visits
 FROM analytics.fact_encounters AS encounters
@@ -15,12 +15,13 @@ JOIN analytics.dim_providers AS providers
     ON encounters.provider_id = providers.id
 GROUP BY 
     providers.id, 
-    providers.name, 
+    providers.full_name, 
     providers.city
 ORDER BY total_patient_visits DESC;
 
 
 SELECT * FROM vw_provider_visits;
 
-DROP VIEW analytics.vw_patient_health_summary;
+
+
 
